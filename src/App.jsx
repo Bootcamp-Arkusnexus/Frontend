562872f54bc2d3b0723ebbb43ce32f22b9e268b3
Sebastian Valenzuela Ojeda <svalenzuela@arkusnexus.com>
2025-01-24 11:50:33 -0800

    fix(API url) API url updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     setLoading(true);
     try {
       const response = await fetch(
-        "https://backend-production-c3c1.up.railway.app/users"
+        "https://backend-production-429b.up.railway.app/users"
       );
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -40,7 +40,7 @@ function App() {
 
     try {
       const response = await fetch(
-        "https://backend-production-c3c1.up.railway.app/users",
+        "https://backend-production-429b.up.railway.app/users",
         {
           method: "POST",
           body: JSON.stringify(userData),
